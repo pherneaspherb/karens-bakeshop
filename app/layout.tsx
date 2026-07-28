31a8e90fb5e090d605d596bb68b73cfb9f9d96e4
[pherneaspherb] <[johnpherloyd.rangas@wvsu.edu.ph]>
2026-07-28 13:11:12 +0800

    chore: moved few folders

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Poppins, Geist } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { cn } from "@/components/lib/utils";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
